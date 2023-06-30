bd0814a442983f6f1a61dc8c34db3c1a1426f523
Alexander Becker <alexander.becker02@sap.com>
2023-06-30 10:48:39 +0200

    add constants and line numbers

--- src/main.c
+++ src/main.c
@@ -5,7 +5,12 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_RETURN, 1);
 
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);    
